ff06d87a10ae1aced8334061f650a1c1bba20d9d
MercyHeart <2384268568@qq.com>
2024-09-12 04:05:00 +0200

    aligned

--- bcj_script.sh
+++ bcj_script.sh
@@ -8,8 +8,7 @@ output_dir_bcj_decompressed=${input_dir}_decompressed
 output_file="file_sizes.txt"
 
 BCJ_EXE=${2:-./x86_bcj_for_script.exe}
-XZ=/mnt/c/Users/mikuk/Desktop/share/ROFS_XMAN/1.bin/bin/xz
-
+# xz=/mnt/c/Users/mikuk/Desktop/share/ROFS_XMAN/1.bin/bin/xz
 
 # 确保输出文件是空的
 > "$output_file"
@@ -35,70 +34,68 @@ for dir in "$input_dir" "$output_dir_bcj"; do
         if [ -f "$file" ]; then
             filename=$(basename "$file")
             original_file_size=$(stat -c%s "$file")
-            output_file_compressed="$output_dir_compressed/$filename.xz"
-            xz -c "$file" > "$output_file_compressed" &
+            output_file_compressed="$output_dir_compressed/$filename.lz4"
+            lz4 -c "$file" > "$output_file_compressed" &
         fi
     done
 done
 wait
 
-for file in "$output_dir_compressed"/*.bcj.xz; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file" .xz)  # 获取去掉 .xz 后缀的文件名
-        decompressed_file="$output_dir_bcj_decompressed/$filename"
+# for file in "$output_dir_compressed"/*.bcj.lz4; do
+#     if [ -f "$file" ]; then
+#         filename=$(basename "$file" .lz4)  # 获取去掉 .lz4 后缀的文件名
+#         decompressed_file="$output_dir_bcj_decompressed/$filename"
 
-        # 解压 .xz 文件
-        xz -d -c "$file" > "$decompressed_file"
+#         # 解压 .lz4 文件
+#         lz4 -d -c "$file" > "$decompressed_file"
 
-        # 用 BCJ_EXE 进行解码
-        output_file_decoded="$output_dir_bcj_decompressed/${filename%.bcj}"
-        ${BCJ_EXE} "$decompressed_file" "$output_file_decoded" 0 &
-    fi
-done
-wait
+#         # 用 BCJ_EXE 进行解码
+#         output_file_decoded="$output_dir_bcj_decompressed/${filename%.bcj}"
+#         ${BCJ_EXE} "$decompressed_file" "$output_file_decoded" 0 &
+#     fi
+# done
+# wait
 
 rm -rf $output_dir_bcj
 
 print_all_result()
 {
-	local filename
-	local file_size
-	local cur_name
-	for f in $(ls $input_dir/*)
-	do
-		cp $f ${output_dir_compressed}
-	done
+    local filename
+    local file_size
+    local cur_name
 
-	# echo "文件名 原始大小 压缩大小 bcj压缩大小"
-	printf "%-45s%-25s%-25s%-25s%-25s\n" "Filename" "Size" "Bcj_Compressed_size" "Compressed_size" "Optimization rate"
-	# printf "%-30s%    -15s%    -15s%    -15s\n" "文件名" "原始大小" "压缩大小" "bcj压缩大小"
-	for f in `ls $input_dir/*`
-	do
-		filename=$(basename $f)
-		# echo -n $filename
-		printf "%-45s" $filename
-		# 清空变量
-    	size_xz=0
-    	size_bcj_xz=0
-		for x in `ls $output_dir_compressed/${filename}* | sort`
-		do
-			file_size=$(stat -c%s "$x")
-			# echo -n " " $file_size
-			printf "%-25s" $file_size
-			if [[ "$x" == *.bcj.xz ]]; then
-            	size_bcj_xz=$file_size
-        	elif [[ "$x" == *.xz ]]; then
-            	size_xz=$file_size
-        	fi
-		done
-		# 计算优化率
-    	if [[ $size_xz -gt 0 ]]; then
-        	rate=$(awk "BEGIN { printf \"%.2f\", ($size_xz - $size_bcj_xz) / $size_xz * 100 }")
-    	else
-        	rate="N/A"
-    	fi	
-		printf "%-25s\n" "$rate%"	
-		echo ""
-	done
+    # echo "文件名 原始大小 压缩大小 bcj压缩大小"
+    printf "|%-50s|%-12s|%-12s|%-12s|%-15s|\n" "Filename" "Size" "Compressed" "Bcj" "Opt_rate"
+    
+    for f in $(ls $input_dir/*); do
+        filename=$(basename "$f")
+        printf "|%-50s" "$filename"
+
+        # 清空变量
+        size_lz4=0
+        size_bcj_lz4=0
+        
+        for x in $(ls $output_dir_compressed/${filename}* | sort); do
+            file_size=$(stat -c%s "$x")
+            if [[ "$x" == *.bcj.lz4 ]]; then
+                size_bcj_lz4=$file_size
+            elif [[ "$x" == *.lz4 ]]; then
+                size_lz4=$file_size
+            fi
+        done
+
+		printf "|%-12s" "$file_size"
+        printf "|%-12s" "$size_lz4"
+        printf "|%-12s" "$size_bcj_lz4"
+
+        # 计算优化率
+        if [[ $size_lz4 -gt 0 ]]; then
+            rate=$(awk "BEGIN { printf \"%.2f\", ($size_lz4 - $size_bcj_lz4) / $size_lz4 * 100 }")
+        else
+            rate="N/A"
+        fi
+        printf "|%-15s|\n" "$rate%"
+    done
 }
 print_all_result
+
